7cf40886471e591ea4208aa09a90da5e2f0b2190
Justin Pretlow <pretlowjc@appstate.edu>
2021-07-28 12:13:21 -0400

    added cad coins

--- CadNickel.java
+++ CadNickel.java
@@ -1,8 +1,7 @@
-public class Loonie extends Coin {
-
-    public Loonie() {
+public class CadNickel extends Coin {
+    public CadNickel() {
         countryCode = "CAD";
-        denomination = 1.00;
+        denomination = 0.05;
     }
 
     public void smelt() {
