8cebe2f995d9b4c4a2cfa049ca6936250cbdf0de
billyjp <billyjp@appstate.edu>
2021-07-28 12:39:07 -0400

    Updated USCoinsTest to use names from coins and mints, added testGetCountryCode

--- USCoinsTest.java
+++ USCoinsTest.java
@@ -3,18 +3,17 @@ import org.junit.jupiter.api.Test;
 
 public class USCoinsTest {
 	
-	USCoinFactory uscf; = USCoinFactory.getInstance();
-
+	USCoinFactory uscf = USCoinFactory.getInstance();
 
 	@Test
 	public void testNullCoin() {};
 
 	// Test all us coins with multiple assertEquals in this method.
 	@Test
-	public void testCoinValue() {
+	public void testGetDenomination() {
 		double[] coinVals = {.01, .05, .1, .25, .5, 1.0};
 		for (double expectedVal : coinVals ) {
-			Coin c = uscf.makeCoin(expectedVal);
+			Coin c = uscf.createCoin(expectedVal);
 			double testVal = c.getValue();
 
 			assertEquals(testVal, expectedVal, 
@@ -23,14 +22,14 @@ public class USCoinsTest {
 		}
 	}
 
-	// Test all cad coins with multiple assertEquals in this method.
+	//Test all US coin names
 	@Test
-	public void testCoinName() {
+	public void testGetName() {
 		double[] coinVals = {.01, .05, .1, .25, .5, 1.0};
 		String[] coinNames = {"Penny", "Nickel", "Dime", "Quarter", "HalfDollar", "Dollar"};
 		for (int i = 0; i < cointVals.length; i++) {
 			String expectedVal = coinNames[i];
-			Coin c = uscf.makeCoin(coinVals[i]);
+			Coin c = uscf.createCoin(coinVals[i]);
 			String testVal = c.getName();
 			 
 			assertEquals(testVal, expectedVal, 
@@ -39,6 +38,20 @@ public class USCoinsTest {
 		}
 	}
 
+    
+    @Test
+    public void testGetCountryCode() {
+        String expectedVal = "USD";
+		double[] coinVals = {.01, .05, .1, .25, .5, 1.0};
+        for (double cV : coinVals) {
+            Coin c = uscf.createCoin(cV);
+            String testVal = c.getCountryCode();
+            
+			assertEquals(testVal, expectedVal, 
+			"Expected: " + expectedVal 
+			+ ", but got: " + testVal);
+        }
+    }
 
-	
 }
+
