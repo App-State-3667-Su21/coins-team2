30a9bab7b63b9bc59c57436b7adffd8935398e3e
Reis Gadsden <gadsdenrm@appstate.edu>
2021-07-28 12:09:29 -0400

    Added smelt test

---
+++ USSmeltTest.java
@@ -0,0 +1,20 @@
+import java;
+
+public class USSmeltTest() {
+	Smelter s = Smelter.getInstance();
+
+	@Test
+	public void testSmelt(){};
+
+	@Test
+	public void testImprint();
+	
+	@Test
+	public void testPolish(){};
+
+	@Test
+	public void testInspection(){};
+
+	@Test
+	public void testSmooth(){};
+}
