471424962ee6ecec1696101a4976a6c7b71bfe73
billyjp <billyjp@appstate.edu>
2021-07-29 13:12:39 -0400

    Made tests for each coin in USDCoinMintTest

--- USDCoinMintTest.java
+++ USDCoinMintTest.java
@@ -3,16 +3,58 @@ import org.junit.jupiter.api.Test;
 
 public class USDCoinMintTest {
 	
-	@Test
-	public void testCoinObj(){
-		USDCoinMint uscm = new USDCoinMint();
+	USDCoinMint uscm;
 
-		double[] denoms = {0.01, 0.05, 0.1, 0.25, 0.5, 1.0};
-		Coin[] coins = {new Penny(), new Nickel(), new Dime(), new Quarter(), new HalfDollar(), new Dollar()};
-		for(int i = 0; i < denoms.length; i++) {
-			Coin c = uscm.createCoin(denom[i]);
-			assertTrue(c instanceof coins[i]);
-		}
+	@BeforeEach
+	public void init() {
+		uscm = new USDCoinMint();
+	}
+
+	// @Test
+	// public void testCoinObj(){
+		
+
+	// 	double[] denoms = {0.01, 0.05, 0.1, 0.25, 0.5, 1.0};
+	// 	Coin[] coins = {new Penny(), new Nickel(), new Dime(), new Quarter(), new HalfDollar(), new Dollar()};
+	// 	for(int i = 0; i < denoms.length; i++) {
+	// 		Coin c = uscm.createCoin(denom[i]);
+	// 		assertTrue(c instanceof coins[i]);
+	// 	}
+	// }
+
+	@Test
+	public void testPenny() {
+		Coin c = uscm.createCoin(.01);
+		assertTrue(c instanceof Penny);
+	}
+
+	@Test void testNickel() {
+		Coin c = uscm.createCoin(.05);
+		assertTrue(c instanceof Nickel);
+	}
+
+	@Test
+	public void testDime() {
+		Coin c  = uscm.createCoin(.10);
+		assertTrue(c instanceof Dime);
+	}
+
+	@Test
+	public void testQuarter() {
+		Coin c = uscm.createCoin(.25);
+		assertTrue(c instanceof Quarter);
+	}
+
+	@Test
+	public void testHalfDollar() {
+		Coin c = uscm.createCoin(.25);
+		assertTrue(c instanceof HalfDollar);
+	}
+
+	@Test 
+	public void testDollar() {
+		Coin c = uscm.createCoin(1.0);
+		assertTrue(c instanceof Dollar);
 	}
 
 //	@Test
