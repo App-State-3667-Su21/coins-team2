722cb78435f1ca7062109ab6dcc5800d95153213
billyjp <billyjp@appstate.edu>
2021-07-29 13:37:05 -0400

    Added very ugly cases for switch statement that catches possible ways of writing values, should change this/standardize how we write denominations

--- USDCoinMint.java
+++ USDCoinMint.java
@@ -15,26 +15,37 @@ public class USDCoinMint extends CoinMint {
 
         switch(denomString) {
             case "1.00":
+            case "1.0":
+            case "1":
                 return new Dollar();
                 //break;
             
             case "0.50":
+            case ".50":
+            case "0.5":
+            case ".5":
                 return new HalfDollar();
                 //break;
 
             case "0.25":
+            case ".25":
                 return new Quarter();
                 //break;
 
             case "0.10":
+            case ".10":
+            case "0.1":
+            case ".1":
                 return new Dime();
                 //break;
 
             case "0.05":
+            case ".05":
                 return new Nickel();
                 //break;
 
             case "0.01":
+            case ".01":
                 return new Penny();
                 //break;
 
