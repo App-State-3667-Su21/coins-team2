d08f2d1499c4b7fae2ba6734c2cd6059f6e6d214
billyjp <billyjp@appstate.edu>
2021-07-30 18:07:19 -0400

    Created NullCoin.java, may need to change returns of getters

---
+++ NullCoin.java
@@ -0,0 +1,22 @@
+public class NullCoin extends Coin {
+
+    public NullCoin() {
+        super(-1);
+    }
+    
+    public String getCountryCode() {
+        return "";
+    }
+
+    public double getDenomination() {
+        return -1;
+    }
+
+    public void smelt() {};
+
+    public void inspect() {};
+
+    public void smooth() {};
+
+    public void buff() {};
+}
